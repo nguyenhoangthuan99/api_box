01b9e8d9ab7574ba31412913216b3459a904ffbd
nguyenhoangthuan99 <thuanhppro12@gmail.com>
2020-10-18 23:01:04 +0700

    report update success

--- main_v4.py
+++ main_v4.py
@@ -3,8 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from modules import cam_login_api
 from modules import decode
 from modules import update
+import requests
+import json
 
 app = FastAPI()
+
 import random
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,6 +27,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("startup")
+async def startup_event():
+    fp = open('config.json', 'r')
+    data = json.load(fp)
+    version = data["version"]
+    update_success = data["update_success"] 
+    fp.close()
+    if update_success:
+        requests.post(url="http://14.177.239.164:9090/updateVersion",json={"AIBOX_ID":"aibox_id_1","status":"","new_version":version})
+    data["update_success"] = False
+    with open("config.json","w") as fp:
+        json.dump(data,fp)  
 
 app.include_router(cam_login_api.router)
 app.include_router(decode.router)
