a4adabc89c5b3b144719b16f384bce9ea1f57d34
mic-710iva <mic-710iva@ubuntu.ubuntu>
2020-11-21 11:40:17 +0800

    fix service and install sh

--- installl.sh
+++ installl.sh
@@ -1,8 +1,14 @@
+cd ~/api_box
+mkdir ~/api_box/TRT_model
+cd ~/api_box/ONNX_model
+wget http://14.177.239.164:7070/model_onnx/ONNX_model/east_288_352.onnx
+wget http://14.177.239.164:7070/model_onnx/ONNX_model/east_704_1280.onnx
+wget http://14.177.239.164:7070/model_onnx/ONNX_model/ocr_10.onnx
 
 sudo nano /etc/pam.d/common-password >> minlen=1 >> passwd
 
 sudo apt-get update
-sudo apt-get install nano python-dev libjpeg-dev libjpeg8-dev libfreetype6-dev libgeos-dev libffi-dev
+sudo apt-get install nano jq python-dev libjpeg-dev libjpeg8-dev libfreetype6-dev libgeos-dev libffi-dev
 
 echo 'export CPATH=$CPATH:/usr/local/cuda-10.2/targets/aarch64-linux/include' >> ~/.bashrc 
 echo 'export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-10.2/targets/aarch64-linux/lib' >> ~/.bashrc 
@@ -17,15 +23,11 @@ unzip model_onnx.zip
 cd model_onnx/ONNX_model
 python3 ocr_.py
 python3 onnx_to_tensorrt.py
-mkdir ~/model_onnx/TRT_model
-mv *.trt ~/model_onnx/TRT_model
+mkdir ~/api_box/TRT_model
+mv *.trt ~/api_box/TRT_model
 
 export DEVICE="jetson"
 echo 'export DEVICE="jetson"' >> ~/.bashrc 
 uvicorn main_v4:app --host 0.0.0.0 --port 8000
 
 gst-launch-1.0 rtspsrc location=rtsp://admin:Camera1234@171.244.236.152:554 latency=0 ! queue max-size-buffers=1 ! rtph264depay ! h264parse ! omxh264dec ! videoconvert ! appsink max-buffers=1 drop=True 
-
-gst-launch-1.0 -v playbin uri=rtsp://admin:Camera1234@171.244.236.152:554 uridecodebin0::source::latency=300
-
-gst-launch-1.0 rtspsrc location=rtsp://admin:Camera1234@171.244.236.152:554 ! queue ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
